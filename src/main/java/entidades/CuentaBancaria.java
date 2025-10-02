d98efe793b6cdc305656ecc750d03178278e9d7b
Sol <montenegrosol260@gmail.com>
2025-10-02 13:27:52 -0300

    a

--- src/main/java/entidades/CuentaBancaria.java
+++ src/main/java/entidades/CuentaBancaria.java
@@ -1,4 +1,39 @@
 package entidades;
 
 public class CuentaBancaria {
+   private double montoTransaccion;
+   private double saldo;
+
+    public CuentaBancaria(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double depositar(double monto){
+        if (saldo > 0){
+            saldo += monto;
+        }
+        return saldo;
+    }
+    public double retirar(double monto){
+        if (monto > 0 && monto <= saldo) {
+            saldo -= monto;
+        }
+        return saldo;
+    }
+
+    public void setMontoTransaccion(double montoTransaccion) {
+        this.montoTransaccion = montoTransaccion;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getMontoTransaccion() {
+        return montoTransaccion;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
 }
--- src/test/java/transaccionesTest.java
+++ src/test/java/transaccionesTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import entidades.CuentaBancaria;
 
@@ -13,10 +12,16 @@ public class transaccionesTest {
     public void setUp(){
         cb = new CuentaBancaria(50.0); //se ejecuta antes de cada prueba
     }
-
+    //Retiro válido
     @Test
     public void testRetirar(){
         cb.retirar(50.0);
-        assertEquals(50.0, cb.getSaldo());
+        assertEquals(0.0, cb.getSaldo());
+    }
+    //Retiro con valor excedente invalido
+    @Test
+    public void testRetirarInsuf(){
+        cb.retirar(60.0);
+        assertEquals(50, cb.getSaldo());
     }
 }
